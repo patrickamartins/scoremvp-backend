187c96ae2749f4f0b6517b87e799cd32d1faf2a1
patrickamartins <patrick.amartins@gmail.com>
2025-05-13 11:54:23 -0300

    Corrige leitura das variáveis de ambiente do banco

--- database.py
+++ database.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-# Configuração do banco de dados usando variáveis de ambiente
-DB_USER = os.environ.get('DB_USER', 'scoremvp_user')
-DB_PASS = os.environ.get('DB_PASS', 'StrongPass!123')
-DB_HOST = os.environ.get('DB_HOST', 'localhost')
-DB_NAME = os.environ.get('DB_NAME', 'scoremvp')
+# Correto: buscar pelo nome da variável de ambiente
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_NAME = os.environ.get("DB_NAME")
 
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:3306/{DB_NAME}"
 
