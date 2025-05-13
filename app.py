13bf76f271986f7eb5bd5288eeaa7f2b8f3b65dd
patrickamartins <patrick.amartins@gmail.com>
2025-05-13 14:29:29 -0300

    ajuste de porta

--- app.py
+++ app.py
@@ -206,5 +206,5 @@ def get_dashboard(current_user):
         db.close()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port) 
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False) 
