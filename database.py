656f5d043db183d6b0c5f6c4ed7d93a59ffde77f
patrickamartins <patrick.amartins@gmail.com>
2025-05-13 12:01:15 -0300

    Corrige conexão com banco usando variáveis padrão do Railway

--- database.py
+++ database.py
@@ -4,13 +4,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-# Correto: buscar pelo nome da variável de ambiente
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
-DB_HOST = os.environ.get("DB_HOST")
-DB_NAME = os.environ.get("DB_NAME")
+# Pegando as variáveis de ambiente corretas
+DB_USER = os.environ.get("MYSQLUSER", "mvpplayer")
+DB_PASS = os.environ.get("MYSQLPASSWORD", "cPUBRDSUdOKvCoUYKdcMxfnpeGpPLNMQ")
+DB_HOST = os.environ.get("MYSQLHOST", "mysql.railway.internal")
+DB_NAME = os.environ.get("MYSQLDATABASE", "scoremvp")
+DB_PORT = os.environ.get("MYSQLPORT", "3306")
 
-SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:3306/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
