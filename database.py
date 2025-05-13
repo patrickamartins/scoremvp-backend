fd7aaee93e317cd815f3c901079be3662148b3e4
patrickamartins <patrick.amartins@gmail.com>
2025-05-13 12:28:06 -0300

    Garante uso de variáveis de ambiente do Railway por referência

--- database.py
+++ database.py
@@ -5,10 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 # Pegando as variáveis de ambiente corretas
-DB_USER = os.environ.get("MYSQLUSER", "mvpplayer")
-DB_PASS = os.environ.get("MYSQLPASSWORD", "cPUBRDSUdOKvCoUYKdcMxfnpeGpPLNMQ")
-DB_HOST = os.environ.get("MYSQLHOST", "mysql.railway.internal")
-DB_NAME = os.environ.get("MYSQLDATABASE", "scoremvp")
+DB_USER = os.environ.get("MYSQLUSER")
+DB_PASS = os.environ.get("MYSQLPASSWORD")
+DB_HOST = os.environ.get("MYSQLHOST")
+DB_NAME = os.environ.get("MYSQLDATABASE")
 DB_PORT = os.environ.get("MYSQLPORT", "3306")
 
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
